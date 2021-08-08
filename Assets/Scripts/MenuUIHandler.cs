e9b4ef1a6501842196d07ee534e04c4b21d14cc0
CaligoArts <CaligoArts@gmail.com>
2021-08-08 11:46:55 -0400

    Simplified scripting & Added Save/ Load Methods
    Removed MySettings.cs and put everything in MenuUIHandler.cs to simplify coding. Everything works with no errors.
    Need to figure out how to load playerName now.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -14,13 +16,25 @@ public class MenuUIHandler : MonoBehaviour
 {
 
     public InputField playername;
+    public static string playernamestr;
+    public Text playerNameDisplay;
 
-    
+    private void Awake()
+    {
+        LoadPlayerName();
+    }
+
+    private void Start()
+    {
+        playerNameDisplay.text = playernamestr;
+        //Need to figure out how to get LoadPlayerName(); working here. started to go this route: if (playernamestr == null) {LoadPlayerName();} 
+
+    }
 
     public void StartNew()
     {
         Debug.Log("Player Name is: " + playername.text);
-        MySettings.playernamestr = playername.text;
+        playernamestr = playername.text;
 
         SceneManager.LoadScene(1);
     }
@@ -32,11 +46,40 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        //SavePlayerName();
+
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
     Application.Quit();
 #endif
+    }
 
+    [System.Serializable]
+    class SaveData
+    {
+        public Text playerNameDisplay;
+    }
+
+    public void SavePlayerName()
+    {
+        SaveData data = new SaveData();
+        data.playerNameDisplay = playerNameDisplay;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadPlayerName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerNameDisplay = data.playerNameDisplay;
+        }
     }
 }
